6dd1895efd2b2dcf9dcfea1088981c0386dce2a8
BashaVali <bashavali222@gmail.com>
2022-12-27 03:18:13 +0530

    Reflection

---
+++ ReflectionDemo/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionDemo
+{
+    internal class Customer
+    {
+        public int a, b;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Customer()
+        {
+            Id = 2;
+            Name = "Vali";
+        }
+        public Customer(int id)
+        {
+            Id = id;
+        }
+        public void Print()
+        {
+
+        }
+        public void Test(int a)
+        {
+
+        }
+    }
+}
